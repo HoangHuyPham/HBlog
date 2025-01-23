cde2457b11cfa4e5fb5affa6521bbe51cac3a9c7
huypham <hoanghuy7284022@gmail.com>
2025-01-23 18:45:20 +0700

    update index

---
+++ src/components/Card.tsx
@@ -0,0 +1,9 @@
+const Card: React.FC = () => {
+    return <>
+        <div className="card col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 bg-white shadow-md h-80 rounded-sm">
+            213
+        </div>
+    </>
+}
+
+export default Card
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        "simple-white": "#f5f5f5",
+        "simple-black": "#4a4a4a",
+      },
+      backgroundColor: {
+        "simple-white": "#f5f5f5",
+      },
+      keyframes: {
+        appear: {
+          from: {
+            transform: "translateY(200%)",
+            opacity: 0,
+          },
+          to: {
+            transform: "translateY(0)",
+            opacity: 1,
+          },
+        },
+      },
+      animation: {
+        appear: "appear forwards .5s ease",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
